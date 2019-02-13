9d7d871c0676050733e41a4a9d52588dff6faa1d
Jantz <jantzillac.93@gmail.com>
2019-02-13 15:53:25 -0600

    Extend "DrawerLayout" & add constructors in "SimpleDrawerLayout"

--- simpledrawerlayout/src/main/java/com/creativesourceapps/android/simpledrawerlayout/SimpleDrawerLayout.java
+++ simpledrawerlayout/src/main/java/com/creativesourceapps/android/simpledrawerlayout/SimpleDrawerLayout.java
@@ -1,4 +1,22 @@
 package com.creativesourceapps.android.simpledrawerlayout;
 
-public class SimpleDrawerLayout {
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
+import android.util.AttributeSet;
+
+public class SimpleDrawerLayout extends DrawerLayout {
+
+    public SimpleDrawerLayout(@NonNull Context context) {
+        super(context);
+    }
+
+    public SimpleDrawerLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SimpleDrawerLayout(@NonNull Context context, @Nullable AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
 }
