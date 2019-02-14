c5926b04a64851ad85a1d321f643740e02979785
Jantz <jantzillac.93@gmail.com>
2019-02-14 15:15:18 -0600

    Override method "addView()"

--- simpledrawerlayout/src/main/java/com/creativesourceapps/android/simpledrawerlayout/SimpleDrawerLayout.java
+++ simpledrawerlayout/src/main/java/com/creativesourceapps/android/simpledrawerlayout/SimpleDrawerLayout.java
@@ -6,6 +6,8 @@ import android.support.annotation.Nullable;
 import android.support.v4.widget.DrawerLayout;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
 public class SimpleDrawerLayout extends DrawerLayout {
@@ -30,5 +32,10 @@ public class SimpleDrawerLayout extends DrawerLayout {
     private void init(Context context, AttributeSet set) {
         LayoutInflater.from(context).inflate(R.layout.simple_drawer_layout, this, true);
         frameLayout = findViewById(R.id.fl_main);
+    }
+
+    @Override
+    public void addView(View child, int index, ViewGroup.LayoutParams params) {
+        super.addView(child, index, params);
     }
 }
